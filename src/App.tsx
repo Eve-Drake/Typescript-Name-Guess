000c53783c31173ef40d8bbbecf341079cbb2ce0
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-09-24 14:17:19 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import {useState} from 'react';
+import './index.css';
+
+interface ageify {
+  age : number,
+  count : number,
+  name : string
+}
 
 function App() {
+  const [name, setName] =  useState<string>('')
+  const [respData, setRespData] = useState<ageify>()
+  const [display, setDisplay] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [statement, setStatement] = useState<string>('Enter a name')
+  
+
+  //Gets data
+  const getNameData = async () =>{
+    setLoading(true)
+    setDisplay(true)
+    if(name){
+      try{
+        const response = await axios.get('https://api.agify.io/?name=' + name)
+        setRespData(response.data)
+        setLoading(false)
+        setStatement(`Ageify guesses that people named ${name} are...`)
+      }
+      catch(err){
+        console.log(err)
+        setLoading(false)
+      }
+    } 
+    else{
+      setDisplay(false)
+      setStatement('Please enter a Name First')
+    }
+
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='nameInput'>
+        {/*Simple Input */}
+        <input 
+          placeholder='Enter a name'
+          value={name}
+          onChange={(e)=>setName(e.target.value)}
+        />
+        <button onClick={getNameData}>Enter</button>
+      </div>
+
+      <div className='nameGuessDisplay'>
+        <h1>{statement}</h1>
+        {/*Only shows if loading is false */}
+        {display && 
+        <div>
+          
+          <h1>{respData?.age} Years Old!</h1>
+        </div>}
+      </div>
+
+      {/*Credit for API */}
+          <div className='footer'>
+            <p>For more infomation, please visit <a href='https://agify.io/'>Ageify.io</a></p>
+          </div>
     </div>
   );
 }
